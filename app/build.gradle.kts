d2a5de5190bbb4c9caa617ba782ac9b5e94d85ea
dush1729 <dush1729@gmail.com>
2025-12-28 22:21:49 +0530

    version upgrade 6(2.1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.dush1729.cfseeker"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "2.0"
+        versionCode = 6
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
